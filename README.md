f910826db58e5f33f2d3dc72384ca2ffe4a7befc
Enricky Hipólito <enricky.hipolito@gmail.com>
2022-05-30 15:38:16 -0300

    corrected build
    added typescript compilation to build
    added '"homepage:" "./"' to package.json

--- README.md
+++ README.md
@@ -5,5 +5,5 @@
 <ul>
   <li>npm start ->  to run the server at http://localhost:3000/</li>
   <li>npm run dev -> to compile TypeScript files to /build in runtime.</li>
-  <li>npm run electron -> electron to open on desktop.</li>
+  <li>npm run electron -> to open on desktop.</li>
 </ul>
--- electron/main.ts
+++ electron/main.ts
@@ -5,7 +5,8 @@ export default path.resolve;
 
 function createWindow() {
   const win = new BrowserWindow({ width: 1200, height: 700 });
-  win.loadURL("http://localhost:3000/");
+  // win.loadFile(path.resolve(__dirname, "index.html")); //for production.
+  win.loadURL("http://localhost:3000/"); //for development.
 }
 
 app.on("ready", createWindow);
